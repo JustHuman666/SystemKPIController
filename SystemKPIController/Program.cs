51b85ebe7dfa295c347b7937207baea813a4bf0f
JustHuman666 <e.myhalchuk@gmail.com>
2020-09-09 12:52:20 +0300

    Initial commit

---
+++ SystemKPIController/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SystemKPIController
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");//here will be our project
+            Console.ReadKey();
+        }
+    }
+}
