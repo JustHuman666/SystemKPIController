63faf967a320bc99aeff8eacae750543b2ecfd35
Nelsoniwe <texno090@gmail.com>
2020-09-09 13:35:45 +0300

    fourth commit

---
+++ SystemKPIController/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SystemKPIController
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");//here will be our project))0)
+            //here will be too
+            Console.ReadKey();
+        }
+    }
+}
