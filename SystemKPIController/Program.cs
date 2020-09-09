c77e74b114371f495d73010b8c237831a28457a3
Nelsoniwe <54241019+Nelsoniwe@users.noreply.github.com>
2020-09-09 13:41:02 +0300

    fifth commit

--- SystemKPIController/Program.cs
+++ SystemKPIController/Program.cs
@@ -6,8 +6,7 @@ namespace SystemKPIController
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");//here will be our project))0)
-            //here will be too
+            Console.WriteLine("Hello World!");//console.WriteLine
             Console.ReadKey();
         }
     }
